338fec87bb1e48006e979767c62e2c2e8c537b18
CarlotadeMiguel <carlota.demiguel@tajamar365.com>
2025-06-09 20:32:52 +0200

    DatabaseSeeder

---
+++ database/factories/PhotoFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Photo>
+ */
+class PhotoFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'url' => $this->faker->imageUrl(),
+            'imageable_id' => 1, // Se ajustará en el seeder
+            'imageable_type' => \App\Models\Restaurant::class,
+        ];
+    }
+}
