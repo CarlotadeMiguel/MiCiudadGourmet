5a86068d017bf83a058d00a1e7bc58ea9ac8101d
CarlotadeMiguel <carlota.demiguel@tajamar365.com>
2025-06-24 16:36:28 +0200

    Corregir Modelos

--- app/Models/ChatConversation.php
+++ app/Models/ChatConversation.php
@@ -26,29 +26,3 @@ class ChatConversation extends Model
     }
 }
 
-// app/Models/ChatMessage.php
-<?php
-
-namespace App\Models;
-
-use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
-
-class ChatMessage extends Model
-{
-    protected $fillable = [
-        'conversation_id',
-        'role',
-        'content',
-        'context_data'
-    ];
-    
-    protected $casts = [
-        'context_data' => 'array'
-    ];
-    
-    public function conversation(): BelongsTo
-    {
-        return $this->belongsTo(ChatConversation::class, 'conversation_id');
-    }
-}
---
+++ app/Models/ChatMessage.php
@@ -0,0 +1,26 @@
+//app/Models/ChatMessage.php
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class ChatMessage extends Model
+{
+    protected $fillable = [
+        'conversation_id',
+        'role',
+        'content',
+        'context_data'
+    ];
+    
+    protected $casts = [
+        'context_data' => 'array'
+    ];
+    
+    public function conversation(): BelongsTo
+    {
+        return $this->belongsTo(ChatConversation::class, 'conversation_id');
+    }
+}
